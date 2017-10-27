9e500aff3dfa160d71aaa96b6164476b41d323ea
Vadim Deryabkin <deryabkinda@mail.ru>
2017-10-27 18:31:01 +0700

    Ядро работает на указателях теперь.

--- core_fsm.cpp
+++ core_fsm.cpp
@@ -1,18 +1,18 @@
 #include "core_fsm.h"
 
 void fsm::start ( void ) const {
-    uint32_t    repeat_loop     = this->vector_fsm[ 0 ].repeat_step;                    // Количество попыток повтора команды в случае неудачи.
-    uint32_t    now_step        = 0;                                                    // Номер текущего шага.
-    uint32_t    previous_step   = 0;                                                    // Предыдущий шаг (из которого пришли в эту функцию).
-    bool        result          = false;                                                // Результат выполнения шага.
+    uint32_t    repeat_loop             = this->vector_fsm[ 0 ].repeat_step;            // Количество попыток повтора команды в случае неудачи.
+    const fsm_step* now_step            = &this->vector_fsm[ 0 ];                       // Структура текущего шага.
+    const fsm_step* previous_step       = &this->vector_fsm[ 0 ];                       // Структура предыдущего шага (из которого пришли в эту функцию).
+    bool        result                  = false;                                        // Результат выполнения шага.
 
     while( true ) {
-        result = this->vector_fsm[ now_step ].func_step( previous_step );               // Выполняем шаг.
+        result = now_step->func_step( previous_step );                                  // Выполняем шаг.
 
         if ( result == true ) {                                                         // В случае успеха переходим на следующий.
             previous_step   = now_step;
-            now_step        = this->vector_fsm[ now_step ].number_step_true;
-            repeat_loop     = this->vector_fsm[ now_step ].repeat_step;
+            now_step        = now_step->number_step_true;
+            repeat_loop     = now_step->repeat_step;
             continue;
         };
 
@@ -23,7 +23,7 @@ void fsm::start ( void ) const {
             continue;
         }
 
-        now_step        = this->vector_fsm[ now_step ].number_step_false;               // Если успеха достичь не удалось.
-        repeat_loop     = this->vector_fsm[ now_step ].repeat_step;
+        now_step        = now_step->number_step_false;                                  // Если успеха достичь не удалось.
+        repeat_loop     = now_step->repeat_step;
     }
 }
--- core_fsm.h
+++ core_fsm.h
@@ -7,9 +7,9 @@
 #define REPEAT_STEP_MAX             0xFFFFFFFF
 
 struct fsm_step {
-    bool ( *func_step )     ( uint32_t previous_step );
-    uint32_t            number_step_true;
-    uint32_t            number_step_false;
+    bool ( *func_step )     ( const fsm_step* previous_step );
+    const fsm_step*     const number_step_true;
+    const fsm_step*     const number_step_false;
     uint32_t            repeat_step;
 };
 
